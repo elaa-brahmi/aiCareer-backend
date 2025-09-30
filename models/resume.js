5f1198180e0dac368bd5d61ef33fb88f428a403b
elaa <elaa.brahmii@gmail.com>
2025-09-30 17:37:12 +0100

    feat : add resume model + upload resumes to supabase + storing resume to db

---
+++ models/resume.js
@@ -0,0 +1,36 @@
+const { DataTypes } = require("sequelize");
+const { sequelize } = require("../config/db");
+const UserModel = require("./user");
+
+const ResumeModel = sequelize.define(
+  "Resume",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    generatedUrl: {
+      type: DataTypes.STRING(500),
+      allowNull: false,
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW,
+    },
+  },
+  {
+    tableName: "resumes",
+    timestamps: false,
+  }
+);
+
+// Define relationship
+ResumeModel.belongsTo(UserModel, { foreignKey: "userId" });
+UserModel.hasMany(ResumeModel, { foreignKey: "userId" });
+
+module.exports = ResumeModel;
