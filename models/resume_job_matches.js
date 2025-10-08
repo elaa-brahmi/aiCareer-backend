bf7ebe415f67ee5c6694206755f1b4ac6261730d
elaa <elaa.brahmii@gmail.com>
2025-10-08 18:13:38 +0100

    feat : add matches table

---
+++ models/resume_job_matches.js
@@ -0,0 +1,75 @@
+const { DataTypes } = require('sequelize');
+const {sequelize,testConnection} = require('../config/db');
+const JobModel = require('./job')
+const UserModel = require('./user')
+const ResumeModel = require('./resume')
+const MatchesJobs = sequelize.define(
+    'matches',
+    {
+        id: {
+          type: DataTypes.BIGINT,
+          primaryKey: true,
+          autoIncrement: true,
+
+        },
+        userId: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+        jobId: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+
+        },
+        resumeId:{
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+        title:{
+            type: DataTypes.STRING(255),
+            allowNull: true,
+
+        },
+        description:{
+            type: DataTypes.STRING(255),
+            allowNull: true,
+
+        },
+        url:{
+            type: DataTypes.STRING(255),
+            allowNull: true,
+
+        },
+        company:{
+            type: DataTypes.STRING(255),
+            allowNull: true,
+
+        },
+        location:{
+            type: DataTypes.STRING(255),
+            allowNull: true,
+
+        },
+        companyLogo:{
+            type: DataTypes.STRING(255),
+            allowNull: true,
+
+        },
+        postedAt:{
+            type: DataTypes.STRING(255),
+            allowNull: true,
+        }
+
+    },
+    {
+        tableName: 'jobMatches',
+        timestamps: false,
+    }
+);
+ResumeModel.belongsTo(UserModel, { foreignKey: "userId" });
+UserModel.hasMany(ResumeModel, { foreignKey: "userId" });
+MatchesJobs.belongsTo(JobModel, { foreignKey: 'jobId' });
+JobModel.hasMany(MatchesJobs, { foreignKey: 'jobId' });
+MatchesJobs.belongsTo(UserModel, { foreignKey: 'userId' });
+UserModel.hasMany(MatchesJobs, { foreignKey: 'userId' });
+module.exports= MatchesJobs
