2c36ac35cf6f8631eb26ede708fc4414613c73e1
elaa <elaa.brahmii@gmail.com>
2025-10-09 20:48:31 +0100

    add express json

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ const {indexJob} = require('./scrapers/linkedinScraper/linkedinService')
 // Increase JSON and URL-encoded body size limit
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
-
+app.use(express.json());
 app.use(
     cors({
       origin: [
