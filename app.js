1f65ff0adf434f578cbbac1e4e29a4d99d87de48
elaa <elaa.brahmii@gmail.com>
2025-10-12 18:15:04 +0100

    add cron jobs

--- app.js
+++ app.js
@@ -6,18 +6,15 @@ const cors = require("cors");
 const {sequelize, testConnection} = require('./config/db');
 const authRouter = require('./routers/authRouter')
 const paymentRouter = require('./routers/paymentRouter')
-const { verifyPlanExpiration } = require("./controllers/userController"); 
+const { verifyPlanExpiration ,resetUserCounters} = require("./controllers/userController"); 
 const CoverLetterRouter = require('./routers/coverLetter')
 const cron = require("node-cron");
 const helmet = require("helmet")
-const {resetMonthlyUploads} = require('./controllers/resumeController')
-const {resetWeeklyCoverLetters} = require('./controllers/coverLetterController')
 const userRouter = require('./routers/userRouter')
 app.use(helmet())
 const scraperRouter = require('./routers/scrapers')
 const {updateJobs} = require('./scrapers/remoteokScraper')
 const {errorHandler} = require('./scrapers/linkedinScraper/errorHandler')
-const {searchJobs} = require('./scrapers/linkedinScraper/jobController')
 const {saveJobs} = require('./scrapers/linkedinScraper/linkedinService')
 const deleteOldJobs = require('./controllers/jobController')
 const {ResumeRouter} = require('./routers/resumeRouter');
@@ -43,19 +40,6 @@ app.use(
       console.error('Sequelize sync failed:', e);
     }
   })();
-  app.get('/', (req, res) => {
-    res.json({
-      message: 'Welcome to LinkedIn Jobs API',
-      endpoints: {
-        search: '/api/search?keywords=react&location=remote&dateSincePosted=past_24h',
-        cron: '/api/cron/search?keywords=react&location=remote&dateSincePosted=past_24h'
-      },
-      rateLimits: {
-        search: '50 requests per day',
-        cron: '1 request per 6 hours'
-      }
-    });
-  });
 app.use('/api/auth', authRouter )
 app.use('/api/v1/payment', paymentRouter)
 app.use('/api/coverLetter',CoverLetterRouter)
@@ -105,33 +89,23 @@ catch(error){
   
 })
 
-// Endpoint to fetch jobs
-// Cron: runs every 3 days
-//cron.schedule("0 0 */3 * *", async () => {
-  cron.schedule('5 12 * * *', async () => {
+// Cron: runs every 3 days to update jobs from remoteOk
+  cron.schedule('0 0 */3 * *', async () => {
   console.log("Cron job running: updating jobs from remoteok...");
   await updateJobs();
 });
-cron.schedule("42 19 * * *", async () => {
+//cron runs daily at 5 am to verify users plans
+cron.schedule("0 0 5 * * *", async () => {
   console.log("Running daily plan expiration cron job...");
   await verifyPlanExpiration();
 });
-cron.schedule('0 0 1 * *', async() => {
-  console.log('Running monthly job at', new Date());
-  await resetMonthlyUploads()
-});
-cron.schedule('0 2 * * 0', async() => {
-  console.log('Running weekly job at', new Date());
-  await resetWeeklyCoverLetters()
+//cron runs at 2 am to reset user counters
+cron.schedule("0 2 * * *", async () => {
+  console.log("Running daily reset check...");
+  await resetUserCounters();
 });
 
-//cron.schedule('0 0 */5 * *', () => {
-/*   cron.schedule('53 18 * * *', async () => {
-  console.log(' Running scheduled for scraping jobs from linkedin...');
-  await runScheduledScrape({ location: '', dateSincePosted: '' });
-}); */
-
-//delete jobs having posted date more than a month 
+//cron run each 2 days to delete jobs having posted date more than a month 
 cron.schedule('0 0 */2 * *', () => {
   console.log(' Running scheduled cleanup for old jobs...');
   deleteOldJobs();
