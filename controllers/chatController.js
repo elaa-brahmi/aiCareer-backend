529ee732aefcef526cf25db69643fe97a5ade372
elaa <elaa.brahmii@gmail.com>
2025-10-19 17:55:12 +0100

    add two function to store and get user chat history

--- controllers/chatController.js
+++ controllers/chatController.js
@@ -1,14 +1,22 @@
 const { getEmbedding } = require("../embedder");
 const pinecone = require("../config/pineconeClient");
+const chatModel = require('../models/chat')
 
 const embed_user_query = async(req,res) => {
     try{
         const {msg} = req.body
         console.log('user input ',msg)
+        //add to chat history
+        await chatModel.create({
+            userId:req.user.id,
+            role:'user',
+            content:msg
+        })
         //embed and do the semantic search
+
         const index = pinecone.Index("rag-jobs");
 
-        const embedding = await getEmbedding(resumeText);
+       /*  const embedding = await getEmbedding(msg);
         console.log("resume Embedding length:", embedding.length);
       
       
@@ -18,11 +26,27 @@ const embed_user_query = async(req,res) => {
           includeMetadata: true,
         });
       
-        return query.matches
+        return query.matches */
+        return res.status(200).json({msg:'msg received'})
 
     }
     catch(error){
         console.log(error)
     }
 }
-module.exports = {embed_user_query}
+const return_user_history = async(req,res) =>{
+    const userId = req.user.id
+    try{
+        const history = await chatModel.findAll({
+            where:{userId:userId},
+            order: [['createdAt', 'DESC']],
+        })
+        return res.status(200).json({history:history})
+    }
+    catch(error){
+        console.log(error)
+        return res.status(400).json({message:error})
+    }
+
+}
+module.exports = {embed_user_query,return_user_history}
