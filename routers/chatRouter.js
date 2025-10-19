7086fd5db8c9fec2e76a5a499f3abd54decdddd7
elaa <elaa.brahmii@gmail.com>
2025-10-19 17:55:19 +0100

    add chat router

--- routers/chatRouter.js
+++ routers/chatRouter.js
@@ -1,7 +1,9 @@
-const {embed_user_query} = require('../controllers/chatController')
+const {embed_user_query,return_user_history} = require('../controllers/chatController')
 const auth = require('../middleware/auth');
 const express = require('express');
 const ChatRouter = express.Router();
 ChatRouter.post('/embed', auth, embed_user_query)
+ChatRouter.get('/all', auth, return_user_history)
+
 module.exports={ChatRouter}
 
