0bd0526c3fba6e7edb679e9763ff816cd071e874
elaa <elaa.brahmii@gmail.com>
2025-10-19 17:54:49 +0100

    add chat model

---
+++ models/chat.js
@@ -0,0 +1,37 @@
+const { DataTypes } = require("sequelize");
+const { sequelize } = require("../config/db");
+
+const chatModel = sequelize.define(
+  "chat",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    role: {
+      type: DataTypes.STRING(255),
+      allowNull: false,
+    },
+    content: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+   
+    createdAt: {
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW,
+    },
+  },
+  {
+    tableName: "chats",
+    timestamps: false,
+  }
+);
+
+
+module.exports = chatModel;
